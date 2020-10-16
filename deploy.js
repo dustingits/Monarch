f5ce88b59e6743c1395514a2bc50804465c2b237
dustin <dustin@swampdogtraining.com>
2020-10-15 22:43:24 -0500

    build

--- deploy.js
+++ deploy.js
@@ -1,9 +1,10 @@
+const {NodeSSH} = require('node-ssh')
 var cmd = require('node-cmd');
 var path, node_ssh, ssh, fs;
 fs = require('fs');
 path = require('path');
 node_ssh = require('node-ssh');
-ssh =  new node_ssh();
+ssh =  new NodeSSH();   
 
 // the method that starts the deployment process
 function main() {
